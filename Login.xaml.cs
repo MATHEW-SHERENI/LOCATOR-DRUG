8aaacff1c122daa015a861ef2748ff6ba7a4f73e
MATHEW-SHERENI <108456079+MATHEW-SHERENI@users.noreply.github.com>
2023-09-14 16:14:26 +0200

    Add files via upload

---
+++ Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Locator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(txtNum.Text=="admin@mathew.com" && txtPassword.Text=="1234")
+            {
+                Navigation.PushAsync(new SearchPage());
+
+            }
+            else
+            {
+                DisplayAlert("Oops","Alert","incorrect details","ok");
+            }
+
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Register());
+        }
+    }
+}
